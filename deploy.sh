75669325080f0b5d37b882f28a9d1d8e53ec81a0
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-06-26 17:44:51 +0530

    Updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,6 @@ if [ $TRAVIS_BRANCH == 'main' ] ; then
   eval "$(ssh-agent -s)"
   ssh-add ~/.ssh/id_rsa
 
-  cd client
   rsync -a .next package.json package-lock.json public travis@167.71.231.248:/home/abhiap/supkenny-site
   echo "Deployed successfully!"
 else
