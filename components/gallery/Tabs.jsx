d66ca4e75418b26b9d9655fed76bc72feeedbe17
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-06-27 23:46:04 +0530

    Changed the gallery tab styles

--- components/gallery/Tabs.jsx
+++ components/gallery/Tabs.jsx
@@ -8,13 +8,13 @@ import ReceptionGallery from "./ReceptionGallery";
 import EventReminder from "./EventReminder";
 
 const tabs = [
-  { label: "Haldi", value: "haldi" },
+  { label: "Mehendi", value: "mehendi" },
   { label: "Wedding", value: "wedding" },
   { label: "Reception", value: "reception" },
 ];
 
 const Tabs = ({ styles }) => {
-  const [activeTab, setActiveTab] = useState("haldi");
+  const [activeTab, setActiveTab] = useState("mehendi");
   return (
     <div>
       <TabManager
@@ -25,7 +25,7 @@ const Tabs = ({ styles }) => {
       />
       <EventReminder styles={styles} event={activeTab} />
 
-      {activeTab === "haldi" && <HaldiGallery styles={styles} />}
+      {activeTab === "mehendi" && <HaldiGallery styles={styles} />}
       {activeTab === "wedding" && <WeddingGallery styles={styles} />}
       {activeTab === "reception" && <ReceptionGallery styles={styles} />}
     </div>
