5b39ca340cf89cc235d24cc7d7f874c625047f56
Abhishek Pednekar <abhi_ap@hotmail.com>
2021-06-30 23:19:18 +0530

    Updated JSON LD schema based on Google's recommendation

--- components/head/HomeHeadLayout.jsx
+++ components/head/HomeHeadLayout.jsx
@@ -28,42 +28,44 @@ const HomeHeadLayout = (props) => {
           type="application/ld+json"
           dangerouslySetInnerHTML={{
             __html: `[
-                {
-                    "@context": "https://schema.org",
-                    "@type": "Event",
-                    "name": "The wedding of Suparna and Kenneth",
-                    "url": "https://supkenny.site",
-                    "startDate": "2021-08-23",
-                    "endDate": "2021-08-23",
-                    "maximumAttendeeCapacity": 50,
-                    "location": {
-                        "@type": "Postal Address",
-                        "streetAddress": "Recca Valley Rd, Thrikkakara, Kakkanad, Kerala",
-                        "postalCode": "682030",
-                        "addressCountry": "India"
-                    },
-                    "eventSchedule": [
-                        {
-                        "name": "Haldi",
-                        "startDate": "2021-08-22",
-                        "startTime": "17:00",
-                        "scheduleTimezone": "IST"
-                        },
-                        {
-                        "name": "Wedding",
-                        "startDate": "2021-08-23",
-                        "startTime": "11:45",
-                        "endTime": "12:15",
-                        "scheduleTimezone": "IST"
-                        },
-                        {
-                        "name": "Reception",
-                        "startDate": "2021-08-23",
-                        "startTime": "17:00",
-                        "scheduleTimezone": "IST"
-                        }
-                    ]
-                }
+              {
+              "@context": "https://schema.org",
+              "@type": "Event",
+              "name": "Suparna and Kenneth's wedding site",
+              "description": "The wedding of Suparna and Kenneth will take place at the RECCAA Club on 23rd August, 2021",
+              "url": "https://supkenny.site",
+              "startDate": "2021-08-23",
+              "endDate": "2021-08-23",
+              "maximumAttendeeCapacity": 50,
+              "location": {
+                "@type": "Place",
+                  "name": "The RECCAA Club",
+                "address": "Recca Valley Rd, Thrikkakara, Kakkanad, Kerala - 682030"
+              },
+              "eventAttendanceMode": "https://schema.org/MixedEventAttendanceMode",
+              "eventStatus": "https://schema.org/EventScheduled",
+              "eventSchedule": [
+                  {
+                  "name": "Haldi",
+                  "startDate": "2021-08-22",
+                  "startTime": "17:00",
+                  "scheduleTimezone": "IST"
+                  },
+                  {
+                  "name": "Wedding",
+                  "startDate": "2021-08-23",
+                  "startTime": "11:45",
+                  "endTime": "12:15",
+                  "scheduleTimezone": "IST"
+                  },
+                  {
+                  "name": "Reception",
+                  "startDate": "2021-08-23",
+                  "startTime": "17:00",
+                  "scheduleTimezone": "IST"
+                  }
+                ]
+              }
             ]`,
           }}
         />
